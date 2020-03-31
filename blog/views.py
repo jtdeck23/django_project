844fbbf57c6e22aaf9e523228bacadd87c2d3520
jtdeck23 <jordandeck060@gmail.com>
2020-03-30 19:53:06 -0500

    added html templates for home and about pages

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Jordan Deck',
+        'title': 'First Blog Post',
+        'content': 'First Post Losers',
+        'date_posted': 'May 1, 2020'
+    },
+    {
+        'author': 'Akacyea Deck',
+        'title': 'Second Blog Post',
+        'content': 'Second Post Losers',
+        'date_posted': 'August 5, 2020'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blag About</h1>')
+    return render(request, 'blog/about.html')
